213a3824154037249c5c968d41acca913dbc1e00
Graham Diehl <graham@apperdashery.net>
2023-05-27 09:20:43 -0400

    initial version

---
+++ CodeChallenge01/CodeChallenge01/CodeChallenge01App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallenge01App.swift
+//  CodeChallenge01
+//
+//  Created by Graham Diehl on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CodeChallenge01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
